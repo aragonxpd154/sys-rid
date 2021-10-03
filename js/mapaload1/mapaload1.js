ee92a8275e7f54d9f0178b340ba7f0cb6d2fddba
Riida-Chan <marcos.silvabl@live.com>
2021-10-03 13:14:30 -0300

    Add files via upload

---
+++ js/mapaload1/mapaload1.js
@@ -0,0 +1,62 @@
+/* 
+Projecto: MapaLoad1.0
+Author: Marcos Silva\n
+Author URL: "http://aragones.esy.es/mapaload1"
+Date: 14/07/2018
+Licence="Creative Commons Attribution 2.0 Unported"
+Licence URL: "https://creativecommons.org/licenses/by/2.0/br/
+*/
+
+function mapInit() {
+	map = new OpenLayers.Map("theMap", {
+		controls:[new OpenLayers.Control.Navigation()],
+			maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+			maxResolution: 156543.0399,
+			numZoomLevels: 19,
+			units: 'm',
+		projection: new OpenLayers.Projection("EPSG:900913"),
+		displayProjection: new OpenLayers.Projection("EPSG:4326")
+	} );
+	osmAddMap_Layers();
+	if (typeof(localStorage.zoom) == 'undefined')
+	{
+		localStorage.lat = localStorage.lon = 0;
+		localStorage.zoom = 3;
+		map.setCenter(new OpenLayers.LonLat(0, 0), 3);
+	}
+	else
+		map.setCenter(new OpenLayers.LonLat(localStorage.lon, localStorage.lat), localStorage.zoom);
+}
+
+// Definindo Layes do mapa
+function osmAddMap_Layers() {
+	layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+	map.addLayer(layerMapnik);
+}
+
+
+function uiInit()
+{
+	if( /Android|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent) ) 
+	{
+		document.body.appendChild(document.getElementById("save"));
+		document.body.appendChild(document.getElementById("theMap"));
+                document.body.removeChild(document.getElementById("controlsTable"));
+	}
+}
+
+function moveMap(dx, dy) {
+	map.moveByPx(dx, dy);
+}
+
+function zoomMap(dz) {
+	map.zoomTo(map.getZoom() + dz);
+}
+
+function persistMapLocation()
+{
+	var lonLat = map.getCenter();
+	localStorage.lat = lonLat.lat;
+	localStorage.lon = lonLat.lon;
+	localStorage.zoom = map.getZoom();
+}
