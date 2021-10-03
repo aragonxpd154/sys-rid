9ff48cddd3fd7a72a061de7b4b20fa43fd85ad2a
Riida-Chan <marcos.silvabl@live.com>
2021-10-03 13:18:32 -0300

    Add files via upload

---
+++ postos/gazeta-vix/index.php
@@ -0,0 +1,259 @@
+<!-- Author: Marcos Silva\n
+Author URL: "http://aragones.esy.es/rid-pro/""
+Date: 09/07/2018
+Licence="Creative Commons Attribution 2.0 Unported"
+Licence URL: "https://creativecommons.org/licenses/by/2.0/br/
+-->
+<?php
+session_start()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv="Content-Type" content='text/html'>
+        <meta name='description' content='Pagina de Consulta Aracê'>
+        <meta name='keyword' content='system, rid-pro, monitoramento, consulta, aracê, posto, aracê, pagina, api'>
+        <meta name='generator' content='RID PRO API'>
+		<script type='text/javascript' src='/js/mapaload1/mapaload1.js'></script>
+		<script type='text/javascript' src='/js/mapaload1/OpenLayers.js'></script>
+		<script type='text/javascript' src='/js/mapaload1/OpenStreetMap.js'></script>
+    
+        <title>| Gazeta Vix |</title>
+
+        <!-- Fontes do Google -->
+
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:300,300i,400i,500,500i,600,600i,700' rel='stylesheet'>
+
+        <!-- Folha de Estilo do Corpo da Página padrão -->
+
+        <link href='/css/font-awesome.min.css' rel='stylesheet'>
+
+        <!-- Reiniciador do CSS -->
+
+        <link href='/css/normalize.css' rel='stylesheet'>
+
+        <!-- Folha de CSS Do Minigram -->
+
+        <link href='/css/milligram.min.css' rel='stylesheet'>
+
+        <!-- Estilo CSS do Corpor PHP e HTML 
+        Cuido com o Escopo desse Styles.css quando realizar o back-end-->
+
+        <link href='/css/styles.css' rel='stylesheet'>
+
+        <!-- [Execute se for libera ao IE Versão >= 9.0]>
+        <[if lt IE 9]>
+        <script src='css/html5'</script>
+        <![endif]-->
+</head>
+        <body> <!-- BGCOLOR='FFFFFF' TEXT='000000' LINK='0000FF' VLINK='0000FF' marginwidth=0 marginheight=0 leftmargin=0 topmargin=0>
+             <table border=0 width=100% border=0 cellpadding=0 cellspacing=0>
+                 <tr><td bgcolor=000000 width =100% height=3 colspan=2><img src='' border=0 width=1 height=3>
+</td></tr> -->
+
+<div class='navbar'>
+                <div class='row'>
+                <div class='column column-30 col-site-title'><img src='/imagens/redegazetaLogo.png' width='140' height='auto' class='site-title float-left'></a>
+</div>
+
+
+
+<div class='column column-50' class='site-title'><a href='#'></a> 
+
+                <!-- Titulo -->
+                <center><h1>TECNOLOGIA</h1>
+                </center>
+
+</div>
+
+<div class='column column-50' style="position:absolute; margin-top:85px; transform:translateX(70%);" ><a href='#'></a> 
+
+<!-- Menu -->
+<nav>
+<ul class='menususpense'>
+
+<li><a href='#'> Início </a></li>
+<li><a href='/postos/'> Região </a>
+
+<ul>
+
+<li><a href='/postos/gazeta-vix/'> Gazeta Vix </a></li>
+<li><a href='/postos/gazeta-norte/'> Gazeta Norte </a></li>
+<li><a href='/postos/gazeta-noroeste/'> Gazeta Noroeste </a></li>
+<li><a href='/postos/gazeta-sul/'> Gazeta Sul </a></li>
+</ul>
+
+</li>
+
+<li><a href='/servicos/'> Serviços </a></li>
+<li><a href='/documentacao/'> Documentação </a></li>
+<li><a href='/contato/'> Contato </a></li>
+
+
+</ul>
+
+</nav>
+
+
+</div>
+
+
+
+<div class ='column column-30'>
+
+    <div class='user-section'><a href='#'>
+        <img src='/imagens/meu_perfil.png' alt='profile photo' class='circle float-left profile-photo' width='50' height='auto'>
+        <div class='username'>
+            <!-- Nome do usuario -->
+               <h4>Olá</h4>
+            <!-- Nome do usuario -->
+            <p><?php echo $_SESSION['user_name'] ?></p>
+            <h6><a href="index.php?logout">Sair</a></h6>
+            <p></p>
+    </div>
+</a>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+<div class='row'>
+    <div id='sidebar' class='column'>
+        <h5>Navegação</h5>
+
+<ul>
+    <li>
+        <a href='\postos\gazeta-vix\'><em class="fa fa-home"></em> Voltar</a></li>
+        
+        <div id="rolagem">
+        <p></p>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+		<li><a href='\postos\gazeta-vix\itanhenga\'><em class="fa fa-file"></em>Itanhenga</a></li>
+		<li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+		<li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <!--
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li>
+        <li><a href='\postos\gazeta-vix\arace\manuais\'><em class="fa fa-file"></em>Fonte Grande</a></li>
+        <li><a href='\postos\gazeta-vix\arace\listagem\'><em class="fa fa-file"></em>Itanhenga</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Jaburuna</a></li>
+        <li><a href='\postos\gazeta-vix\'><em class="fa fa-file"></em>Kautsky</a></li> -->
+    </div>
+            
+</ul>
+</div>
+<section id='main-content' class='column column-offset-20'>
+    <div class='row grid-responsive'>
+        <div class='column page-heading'>
+            <div class='large-card'>
+                
+<!-- Tabela -->
+<a class 'anchor' name='tables'></a>
+<div class 'row grid-responsive'>
+    <div class='column'>
+        <div class='card'>
+            <div class='card-title'>
+			<div class='badge float-right'>Sede Vitória , Rua: Chafic Murad, nº 902 - Monte Belo - ES, 29053-315, Brasil</div>
+<!-- Titulo -->
+
+<h3>Rede Gazeta Vitória</h3>
+<p></p>
+<h4>Selecione o posto conveniado no menu ao lado</h4>
+<p></p>
+
+<div>
+
+<img width='440' height='600' frameborder='0,5' src='/imagens/vix.png' alt='Rede Gazeta Vitória' title=''/>
+
+<img width="440" height="600" frameborder="0.5" src='/imagens/rede-vix.jpg'/>
+
+</div>
+</tbody>
+</table>
+			<p class="credit">Copyright - <a href="http://aragones.esy.es/rid-pro/">A Gazeta Rádio e Tv Ltd</a></p>
+		</section>
+	</div>
+
+
+<script src='/js/chart.min.js'></script>
+<script src='/js/chart-data.js'></script>
+<script>
+	window.onload = function () {
+		var chart1 = document.getElementById("line-chart").getContext("2d");
+		window.myLine = new Chart(chart1).Line(lineChartData, {
+		responsive: true,
+		scaleLineColor: "rgba(0,0,0,.2)",
+		scaleGridLineColor: "rgba(0,0,0,.05)",
+		scaleFontColor: "#c5c7cc"
+		});
+		var chart2 = document.getElementById("bar-chart").getContext("2d");
+		window.myBar = new Chart(chart2).Bar(barChartData, {
+		responsive: true,
+		scaleLineColor: "rgba(0,0,0,.2)",
+		scaleGridLineColor: "rgba(0,0,0,.05)",
+		scaleFontColor: "#c5c7cc"
+		});
+		var chart4 = document.getElementById("pie-chart").getContext("2d");
+		window.myPie = new Chart(chart4).Pie(pieData, {
+		responsive: true,
+		segmentShowStroke: false
+		});
+		var chart5 = document.getElementById("radar-chart").getContext("2d");
+		window.myRadarChart = new Chart(chart5).Radar(radarData, {
+		responsive: true,
+		scaleLineColor: "rgba(0,0,0,.05)",
+		angleLineColor: "rgba(0,0,0,.2)"
+		});
+		var chart6 = document.getElementById("polar-area-chart").getContext("2d");
+		window.myPolarAreaChart = new Chart(chart6).PolarArea(polarData, {
+		responsive: true,
+		scaleLineColor: "rgba(0,0,0,.2)",
+		segmentShowStroke: false
+		});
+	};
+	</script>
+	</body>
+	</html>
+
