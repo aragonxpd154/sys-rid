380c122171d01cc77ac850a73c84c7b13aaed45c
Riida-Chan <marcos.silvabl@live.com>
2021-10-03 13:03:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!--
+Author Name: Marcos Silva
+Author URL: http://aragones.esy.es/
+License: Creative Commons Attribution 2.0 Unported
+License URL: http://creativecommons.org/licenses/by/2.0/
+--!>
+
+<!-- 
+
+	============================================================
+	Index.php Para redirecionar ao diretório de /consulta
+	============================================================
+--!>
+
+<?php
+header("Location: http://localhost/postos/");
+?>
